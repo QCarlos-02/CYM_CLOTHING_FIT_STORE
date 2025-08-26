e05589341f883acd532c6841d727ba5c0b8c440d
QCarlos-02 <cmanuelquintero@unicesar.edu.co>
2025-08-26 18:25:47 -0500

    _Para_despliegue

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://zhbifkakuklgodydwora.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoYmlma2FrdWtsZ29keWR3b3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NTc5MTUsImV4cCI6MjA3MDUzMzkxNX0.VHs3xr6P4mbkYxy0zcKsSBX-UdmCk_nkL1RAEHuqxRg";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
